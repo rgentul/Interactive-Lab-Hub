725ef22fdec552c7ebb7eeae03c5e6dfa7e5085f
rgentul <66213163+rgentul@users.noreply.github.com>
2021-03-21 19:52:12 -0400

    Update wizard.py

--- Lab 3/wizard.py
+++ Lab 3/wizard.py
@@ -101,7 +101,7 @@ def run_example():
         if my_button.is_button_pressed() == True:
             print("\nThe button is pressed!")
 
-        else: 
+        if my_button.is_button_pressed() == False: 
             y = top
             draw.rectangle((0, 0, width, height), outline=0, fill=0)
             draw.text((x,y), "The button is not pressed!", font=font, fill="#FFFFFF")
