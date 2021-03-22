188f3852065e4d3e230a663afbc1b2b698d74518
rgentul <66213163+rgentul@users.noreply.github.com>
2021-03-22 09:48:57 -0400

    Update wizard.py

--- Lab 3/wizard.py
+++ Lab 3/wizard.py
@@ -88,7 +88,7 @@ def speak(command):
     
 def start():
     if not os.path.exists("model"):
-        print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unp$    
+        print ("Please download the model from https://github.com/alphacep/vosk-api/blob/master/doc/models.md and unpack as 'model' in the current folder.")
         exit (1)
 
     wf = wave.open(sys.argv[1], "rb")
