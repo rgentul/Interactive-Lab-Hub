5c85a1d724cc09107f51c59e9c462193e654c9a5
rgentul <66213163+rgentul@users.noreply.github.com>
2021-04-02 18:20:22 -0400

    Update twist_test.py

--- Lab 4/twist_test.py
+++ Lab 4/twist_test.py
@@ -44,9 +44,75 @@ import qwiic_twist
 import time
 import random
 
+from datetime import datetime, timedelta
+import subprocess
+import digitalio
+import board
+from time import strftime, sleep
+from PIL import Image, ImageDraw, ImageFont
+from adafruit_rgb_display.rgb import color565
+import adafruit_rgb_display.st7789 as st7789
+import webcolors
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(
+    spi,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
+    width=135,
+    height=240,
+    x_offset=53,
+    y_offset=40,
+)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+rotation = 90
+height = disp.width  # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new("RGB", (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height - padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
 def runExample():
 
-	print("\nSparkFun qwiic Twist   Example 2 - crazy colors\n")
+	print("\nStarting program\n")
 	myTwist = qwiic_twist.QwiicTwist()
 
 	if myTwist.connected == False:
@@ -62,7 +128,15 @@ def runExample():
 			"YES" if myTwist.pressed else "NO", \
 			))
 
-		myTwist.set_color( random.randint(0,256), random.randint(0,256),random.randint(0,256))
+		#myTwist.set_color( random.randint(0,256), random.randint(0,256),random.randint(0,256))
+		 draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    		#TODO: fill in here. You should be able to look in cli_clock.py and stats.py
+	   	y = top
+	    	draw.text((x,y), "Turn dial to set", font=font, fill="#FFFFFF")
+
+	    	# Display image.
+	    	disp.image(image, rotation)
 
 		time.sleep(.3)
 
