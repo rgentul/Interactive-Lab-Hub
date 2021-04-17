0491564ae2246d3dba77469138998ae733194d3d
rgentul <66213163+rgentul@users.noreply.github.com>
2021-04-17 18:43:39 -0400

    Update tensor_ex.py

--- Lab 5/tensor_ex.py
+++ Lab 5/tensor_ex.py
@@ -9,7 +9,7 @@ import sys
 import pygame
 
 
-# pygame.mixer.init()
+pygame.mixer.init()
 pygame.mixer.music.load('ding.wav')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy() == True:
